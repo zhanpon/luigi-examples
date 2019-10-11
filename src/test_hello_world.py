2f204f0a6780b7901684b0a00a93ac2599b38e32
Yiyang Zhan <pon.zhan@gmail.com>
2019-10-11 23:32:55 +0900

    Add failing test

--- src/test_hello_world.py
+++ src/test_hello_world.py
@@ -16,3 +16,8 @@ def test_output():
     with mock_target.open("r") as i:
         result = i.readlines()
         assert result == ["Hello, World\n", "Salut !\n"]
+
+
+def test_addition():
+    n = 1 + 1
+    assert n == 1
